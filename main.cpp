cfbe1d4d7d5216b3e6ecdcf4188f5e40fab72563
Sohaib Ali <alisohaibf@gmail.com>
2026-07-24 17:22:56 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "===================================" << endl;
+    cout << "          System Information          " << endl;
+    cout << "===================================" << endl;
+    cout <<"\n\n";
+
+    cout << "Computer Name: ";
+    cout << "Operating System: ";
+    cout << "CPU: ";
+    cout << "RAM: \n\n";
+
+}
+
