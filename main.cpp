a80b42037307ad9157a52a510e87338061aeb0c4
Sohaib Ali <alisohaibf@gmail.com>
2026-08-01 12:56:55 -0400

    Working on storage info, which displays total storage, used storage and available storage

--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include "generalInfo.hpp"
+#include "storageInfo.hpp"
 using namespace std;
 
 int main(){
     GeneralInfo generalInfo;
+    StorageInfo storageInfo;
     cout << "===================================" << endl;
     cout << "         System Information          " << endl;
     cout << "===================================" << endl;
     cout << "\n\n";
     generalInfo.displayGeneralInfo();
+    storageInfo.displayStorageSpace();
+
 }
 
---
+++ storageInfo.cpp
@@ -0,0 +1,16 @@
+#include <sys/statvfs.h>
+#include <iostream>
+#include "storageInfo.hpp"
+using namespace std;
+
+void StorageInfo::displayStorageSpace(){
+    struct statvfs stat;
+
+    totalSpace = stat.f_frsize * stat.f_blocks;
+    freeSpace = stat.f_frsize * stat.f_bavail;
+    usedSpace = totalSpace - freeSpace;
+
+    cout << totalSpace;
+
+
+}
