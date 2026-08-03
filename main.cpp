af0c92644ff68009575671a0b7d982f93b13827c
Sohaib <alisohaibf@gmail.com>
2026-08-03 15:42:49 -0400

    debugging networkInfo.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 #include "generalInfo.hpp"
 #include "storageInfo.hpp"
+#include "networkInfo.hpp"
 using namespace std;
 
 int main(){
     GeneralInfo generalInfo;
     StorageInfo storageInfo;
+    NetworkInfo networkInfo;
     cout << "===================================" << endl;
     cout << "         System Information          " << endl;
     cout << "===================================" << endl;
     cout << "\n";
-    generalInfo.displayGeneralInfo();
-    storageInfo.displayStorageSpace();
 
+    //generalInfo.displayGeneralInfo();
+    //storageInfo.displayStorageSpace();
+    networkInfo.displayNetworkInfo();
 }
 
