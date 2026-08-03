3eb39443c3826f61d7338d9cd4f5b401fbd6b075
Sohaib <alisohaibf@gmail.com>
2026-08-03 15:21:11 -0400

    Beginning to write code for network info, which will include data like ip and mac addresses

---
+++ networkInfo.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <cstdlib>
+#include <iostream>
+
+#include "networkInfo.hpp"
+using namespace std;
+
+void NetworkInfo::displayNetworkInfo(){
+    string ipAddress = "";
+    char buffer[128];
+    
+
+}
+
---
+++ networkInfo.hpp
@@ -0,0 +1,9 @@
+#include <string>
+class NetworkInfo{
+    public:
+        void displayNetworkInfo();
+    
+    private:
+        std::string ipAddress;
+        std::string macAddress;
+};
